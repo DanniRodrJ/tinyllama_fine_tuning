595c2ee3fd40bca7f0985ad31fc3d560f7d2e68a
DanniRodrJ <dannielarodriguezjove@gmail.com>
2025-11-14 08:57:51 -0400

    fix: skip file logging

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -3,6 +3,16 @@ import time
 from typing import Callable, Any
 import functools
 from config.config import TravelAssistantConfig as Config
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+LOG_DIR = BASE_DIR / "logs"
+LOG_DIR.mkdir(parents=True, exist_ok=True)
+
+if Config.SHOW_LOGGING and Config.LOGGIN_TXT:
+    LOG_FILE = LOG_DIR / "logfile.txt"
+else:
+    LOG_FILE = LOG_DIR / "app.log"
 
 def setup_logger(name: str, log_file: str = 'app.log', level: int = logging.INFO, enable: bool = True) -> logging.Logger:
 
@@ -17,13 +27,14 @@ def setup_logger(name: str, log_file: str = 'app.log', level: int = logging.INFO
         datefmt='%Y-%m-%d %H:%M:%S'
     )
     
-    file_handler = logging.FileHandler(log_file, mode='w', encoding='utf-8')
-    file_handler.setFormatter(formatter)
-    
+    if enable:
+        file_handler = logging.FileHandler(log_file, mode='w', encoding='utf-8')
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+        
+        
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
-    
-    logger.addHandler(file_handler)
     logger.addHandler(console_handler)
     
     return logger
@@ -34,15 +45,12 @@ def timer_decorator(logger: logging.Logger = None):
         @functools.wraps(func)
         def wrapper(*args, **kwargs) -> Any:
             start_time = time.time()
-            
             result = func(*args, **kwargs)
-            
             elapsed_time = time.time() - start_time
             minutes, seconds = divmod(elapsed_time, 60)
-            
             message = f"Función '{func.__name__}' ejecutada en: {int(minutes)}m {seconds:.2f}s"
             
-            if logger:
+            if logger and logger.isEnabledFor(logging.INFO):
                 logger.info(message)
             else:
                 print(message)
@@ -51,6 +59,10 @@ def timer_decorator(logger: logging.Logger = None):
         return wrapper
     return decorator
 
-LOG_FILE = 'logfile.txt' if Config.SHOW_LOGGING and Config.LOGGIN_TXT else 'logs/app.log'
-app_logger = setup_logger('fine_tuning_llama', LOG_FILE, logging.INFO, enable=Config.SHOW_LOGGING)
+app_logger = setup_logger(
+    name='fine_tuning_llama',
+    log_file=str(LOG_FILE), 
+    level=logging.INFO,
+    enable=Config.SHOW_LOGGING 
+)
 
